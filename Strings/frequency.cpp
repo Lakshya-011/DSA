174d2c051568b1e5c15a1975c5edab2a1bdf906a
Lakshya-011 <lakshyasri1306@gmail.com>
2025-11-04 15:31:46 +0530

    count frequency of characters in string

---
+++ Strings/frequency.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "LakshyaSrivastava";
+    unordered_map<char, int> freq;
+    for (char c : s) freq[c]++;
+    for (auto p : freq) cout << p.first << ":" << p.second << endl;
+    return 0;
+}
