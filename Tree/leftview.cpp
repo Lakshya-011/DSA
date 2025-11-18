ce5b26b28dd92aa9963c1c28f4d28842fa5cc4f9
Lakshya-011 <lakshyasri1306@gmail.com>
2025-11-18 10:13:59 +0530

    Left View of tree

--- Tree/leftview.cpp
+++ Tree/leftview.cpp
@@ -18,8 +18,8 @@ void solve(Node* root, int level,vector<int>& ans){
         if(level==ans.size())
         ans.push_back(root->data);
 
-        solve(root->right,level+1,ans);
         solve(root->left,level+1,ans);
+        solve(root->right,level+1,ans);
     }
 Node* buildtree(){
     int x;
